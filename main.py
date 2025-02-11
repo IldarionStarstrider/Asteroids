2b8811ff27e75c371dc3fa86ec2a65873c11bbc5
IldarionStarstrider <harm.denhertog@hotmail.com>
2025-02-11 10:22:17 +0100

    Added Clock object and dt variable. added tick() method to gameloop.

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from constants import *
 
 def main():
     pygame.init()
+    Clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-
+        Clock.tick(60)
+        dt = (Clock.tick(60) / 1000)
         pygame.display.flip()
 
 
