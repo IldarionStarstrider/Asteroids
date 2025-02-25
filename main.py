4cd22f3b2385dacb6ad92cba6fa30253a8edfed6
IldarionStarstrider <harm.denhertog@hotmail.com>
2025-02-25 11:57:00 +0100

    Created Asteroids.py and added Asteroid class; Created asteroidfield.py and copied in provided code; updated main.py to import from asteroid and asteroidfield; added new containers in main.py for asteroid and asteroid field.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-#importing pygame:
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+
     dt = 0
 
     while True:
@@ -15,9 +28,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        updateable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         
         # limit the framrate to 60
