ba0fc1e557dd079d85cf649f6d168f20ee26f0fe
IldarionStarstrider <harm.denhertog@hotmail.com>
2025-02-25 08:22:04 +0100

    got Player movement working!

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 #importing pygame:
 import pygame
 from constants import *
-from player import *
-
-gamer = player(
-    SCREEN_WIDTH / 2,
-    SCREEN_HEIGHT / 2,
-     0)
+from player import Player
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        gamer.draw(screen)
-        Clock.tick(60)
-        dt = (Clock.tick(60) / 1000)
+        
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
-
+        
+        # limit the framrate to 60
+        dt = (Clock.tick(60) / 1000)
+        
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,14 +1,13 @@
 import pygame
-from circleshape import *
+from circleshape import CircleShape
 from constants import *
 
 # class defining the player.
-class player(CircleShape):
-    def __init__(self, x, y, radius):
+class Player(CircleShape):
+    def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-    rotation = 0
+        self.rotation = 0
 
-    # in the player class; Code copied from boot.dev
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,10 +17,23 @@ class player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        white = pygame.Color(255, 255, 255) 
-        try:
-            pygame.draw.polygon(screen, white, self.triangle(), 2)
-        except ValueError:
-            print("Not enough positions")
-        except TypeError:
-            print("points are not a sequence or do not contain pairs")
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
